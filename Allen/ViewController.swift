a8b102bebac3acafe3932882913e335f4d7f8125
Shunsuke Sato <shunsukesato@ShunsukenoMacBook-Pro.local>
2016-12-20 00:08:27 +0900

    try example

--- Allen/ViewController.swift
+++ Allen/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let message = "Hello!"
     }
 
     override var representedObject: Any? {
--- AllenExtension/SourceEditorCommand.swift
+++ AllenExtension/SourceEditorCommand.swift
@@ -11,8 +11,23 @@ import XcodeKit
 
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
+    // Ref: http://dev.classmethod.jp/smartphone/iphone/xcode-source-editor-extension-how-to-create/
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
-        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        let textBuffer = invocation.buffer
+        
+        let lines = textBuffer.lines
+        let selections = textBuffer.selections
+        
+        guard let selection = selections.firstObject as? XCSourceTextRange else {
+            completionHandler(NSError(domain: "SampleExtension", code: 401, userInfo: ["reason": "text not selected"]))
+            return
+        }
+        
+        let line = selection.end.line
+        let end = selection.end.column
+        
+        let indentSpace = String(repeating: " ", count: end)
+        lines.insert("\(indentSpace)let message = \"Hello!\"", at: line)
         
         completionHandler(nil)
     }
