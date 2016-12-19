1860d8eed5b0e9e609a6992734d86fb38d6a8036
Shunsuke Sato <shunsukesato@ShunsukenoMacBook-Pro.local>
2016-12-19 23:55:21 +0900

    Initial Commit

---
+++ Allen/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Allen
+//
+//  Created by Shunsuke Sato on 2016/12/19.
+//  Copyright © 2016年 Shunsuke Sato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Allen/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Allen
+//
+//  Created by Shunsuke Sato on 2016/12/19.
+//  Copyright © 2016年 Shunsuke Sato. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
