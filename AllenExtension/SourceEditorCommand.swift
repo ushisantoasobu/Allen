5f28cb2ab57cf0b0c331a4001205a88340f7d656
ushisantoasobu <babblemann.shunsee@gmail.com>
2016-12-20 14:43:23 +0900

    temporary impl

--- AllenExtension/SourceEditorCommand.swift
+++ AllenExtension/SourceEditorCommand.swift
@@ -14,6 +14,7 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     // Ref: http://dev.classmethod.jp/smartphone/iphone/xcode-source-editor-extension-how-to-create/
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         let textBuffer = invocation.buffer
+
         
         let lines = textBuffer.lines
         let selections = textBuffer.selections
@@ -24,12 +25,30 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
         }
         
         let line = selection.end.line
+        let start = selection.start.column
         let end = selection.end.column
-        
-        let indentSpace = String(repeating: " ", count: end)
-        lines.insert("\(indentSpace)let message = \"Hello!\"", at: line)
-        
+
+        var targetLineString = lines[line] as! String
+
+        let startIndex = targetLineString.index(targetLineString.startIndex, offsetBy: start)
+        let endIndex = targetLineString.index(targetLineString.startIndex, offsetBy: end)
+        let range = startIndex..<endIndex
+        let selectedString = targetLineString.substring(with: range)
+
+//        lines[line] = targetLineString.replaceSubrange(range, with: self.snake2Camel(string: selectedString))
+        lines[line] = targetLineString.replacingCharacters(in: range, with: self.snake2Camel(string: selectedString))
+
         completionHandler(nil)
+    }
+
+    // Ref: https://github.com/speee/jsonschema2swift/
+    private func snake2Camel(string: String) -> String{
+        let items = string.components(separatedBy: "_")
+        var camelCase = ""
+        items.enumerated().forEach {
+            camelCase += 0 == $0 ? $1.lowercased() : $1.capitalized
+        }
+        return camelCase
     }
     
 }
